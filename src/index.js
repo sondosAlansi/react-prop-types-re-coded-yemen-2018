9bc51f5be69b8dbb8297ec7425ebcde17785fd9d
sondos alansi <sondos.alansi@yahoo.com>
2018-10-23 10:01:57 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // Code Goes Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+class Order extends React.Component {
+ 
+  render() {
+    return (
+      <div className="order">
+        <ul>
+          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
+          <li>{this.props.size}</li>
+          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
+          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
+        </ul>
+      </div>
+    );
+  }
+}
